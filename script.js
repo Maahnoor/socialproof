47cf0af7819a8c9550b1f45b6c76ee0c94a837d5
Maahnoor <maahnoork22@gmail.com>
2022-01-10 14:55:50 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const imid= document.getElementsByClassName('star');
+for(let i=0;i<5;i++)
+{
+	imid[0].innerHTML+=`<img src="images/icon-star.svg" alt="star" style="padding-right: 10px;">`;
+	imid[1].innerHTML+=`<img src="images/icon-star.svg" alt="star" style="padding-right: 10px;">`;
+	imid[2].innerHTML+=`<img src="images/icon-star.svg" alt="star" style="padding-right: 10px;">`;
+
+}
